8f5f5ea694f067fa22e81be63c9128056f0cda28
s13588 <s13588@pjwstk.edu.pl>
2024-10-19 12:26:36 +0200

    Added BarrellRoll and UI
    Added BarrellRoll script logic, and UI placeholder prototype

---
+++ Assets/BarrellMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrellMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float barrellSpeed = 1f;
+
+    [SerializeField]
+    private Rigidbody2D barrellBody;
+
+    [SerializeField]
+    private float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        barrellBody = GetComponent<Rigidbody2D>();
+
+        barrellBody.angularVelocity = barrellSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        barrellBody.angularVelocity = barrellSpeed;
+    }
+    void BarrellCollision2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D playerCollider = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (playerCollider != null)
+            {
+                Vector2 pushDir = collision.transform.position-transform.position;
+                pushDir.Normalize();
+                playerCollider.AddForce(pushDir * force, ForceMode2D.Force);
+            }
+
+        }
+    }
+}
