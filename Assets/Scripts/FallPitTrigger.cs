38071d1bc278288183a95468db23bdfcfa2bbba1
mikolaj <jaworowskimikolaj@gmail.com>
2024-10-20 18:03:50 +0200

    feat: Add FallPitTrigger and prefab

---
+++ Assets/Scripts/FallPitTrigger.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class FallPitTrigger : MonoBehaviour
+{
+    public GameObject respawnPoint;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            this.RespawnPlayer();
+        }
+        else
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void RespawnPlayer()
+    {
+        var playerGameObject = GameObject.FindGameObjectWithTag("Player");
+
+        if (playerGameObject == null)
+        {
+            return;
+        }
+
+        if (respawnPoint != null)
+        {
+            playerGameObject.transform.position = respawnPoint.transform.position;
+            return;
+        }
+        
+        var fallbackRespawnPoint = GameObject.FindGameObjectWithTag("Respawn");
+
+        if (fallbackRespawnPoint == null)
+        {
+            Destroy(playerGameObject);
+            return;
+        }
+            
+        playerGameObject.transform.position = fallbackRespawnPoint.transform.position;
+    }
+}
