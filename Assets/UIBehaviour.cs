bae395e7efab48f2cc0d2625454c2fc247ca7ecb
s13588 <s13588@pjwstk.edu.pl>
2024-10-26 00:00:29 +0200

    changed UI
    changed script and UI.
    
    TODO fix displaying HP

--- Assets/UIBehaviour.cs
+++ Assets/UIBehaviour.cs
@@ -9,44 +9,91 @@ using static UnityEditor.Timeline.TimelinePlaybackControls;
 public class UIBehaviour : MonoBehaviour
 {
     [SerializeField]
-    int lives = 3;
+    int hp;
+
     [SerializeField]
-    TextMeshProUGUI livesDisplay;
+    int lives;
     
     [SerializeField]
     int coins = 0;
 
     [SerializeField] 
     TextMeshProUGUI coinsDisplay;
+   
+    public Image[] hpImages;
+    public Sprite hpActive;
+    public Sprite hpDisable;
 
-   void Start()
+    void Start()
    {
-    livesDisplay.text = lives.ToString();
-    livesDisplay.text = coins.ToString();
+        coinsDisplay.text = coins.ToString();
    }
-   public void antiDuplicateUI(){
+    private void Update()
+    {
+        LivesCountCheck();
+        for (int i = 0; i < hpImages.Length; i++)
+        {
+            disableHeartIcon(i);
+            useSpriteIcon(i);
+        }
+    }
+
+    private void useSpriteIcon(int i)
+    {
+        if (i < hp)
+        {
+            hpImages[i].sprite = hpActive;
+        }
+        else
+        {
+            hpImages[i].sprite = hpDisable;
+        }
+    }
+
+    private void disableHeartIcon(int i)
+    {
+        if (i < lives)
+        {
+            hpImages[i].enabled = false;
+        }
+        else
+        {
+            hpImages[i].enabled = false;
+        }
+    }
+
+    private void LivesCountCheck()
+    {
+        if (hp > lives)
+        {
+            hp = lives;
+        }
+    }
+
+    public void addCoins(int coinsAdd)
+    {
+        coins += coinsAdd;
+        coinsDisplay.text = coins.ToString();
+    }
+    public void antiDuplicateUI(){
     int playerUINumber = FindObjectsOfType<UIBehaviour>().Length;
     if(playerUINumber <1){
         DontDestroyOnLoad(gameObject);
     }else{
         Destroy(gameObject);
     }
-   }
+}
 
    public void Death(){
-    if(lives > 1){
-        lives--;
+        if(lives > 1)
+        {
         int sceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(sceneIndex);
-        livesDisplay.text = coins.ToString();
-    }else{
+        }
+        else
+        {
         SceneManager.LoadScene(0);
         Destroy(gameObject);
-    }
-   }
-   public void addCoins(int coinsAdd)
-   {
-    coins +=coinsAdd;
-    coinsDisplay.text = coins.ToString();
+        }
    }
 }
