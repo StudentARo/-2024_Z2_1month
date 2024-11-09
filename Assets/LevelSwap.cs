10667c232edb1236c36d8d47c9118e48fa04d08a
s13588 <s13588@pjwstk.edu.pl>
2024-11-09 14:58:53 +0100

    added resetting level
    Actually to change scene we need to move to object

--- Assets/LevelSwap.cs
+++ Assets/LevelSwap.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelSwap : MonoBehaviour
 {
-  
     private void OnTriggerEnter2D(Collider2D other)
     {
        //checking Player Tag
@@ -17,7 +16,36 @@ public class LevelSwap : MonoBehaviour
             int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 
             // load next scene
-            SceneManager.LoadScene(currentSceneIndex + 1);
+           // SceneManager.LoadScene(currentSceneIndex + 1);
+            int sceneCount = SceneManager.sceneCountInBuildSettings;
+            if (currentSceneIndex + 1 < sceneCount)
+            {
+                Invoke("LoadNextScene", 2f); // reload scene delay
+            }
+            else
+            {
+                Debug.Log("Nie ma nastêpnej sceny!");
+            }
         }
+    }
+
+    // Funkcja do za³adowania nastêpnej sceny
+    private void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) // Getting button R
+        {
+            RestartScene(); // Restart scene
+        }
+    }
+    private void RestartScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex); // load actual scene
     }
 }
